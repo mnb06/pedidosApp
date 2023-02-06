626458a6ae5c3712803b0b3455148bdfc0dedc43
Matias Bue <matias.bue@outlook.com>
2023-02-06 16:29:36 -0300

    agregada funcion de edicion de clientes

---
+++ app/src/main/java/com/example/pedidosapp/ClientEdit.java
@@ -0,0 +1,98 @@
+package com.example.pedidosapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.pedidosapp.tabs.Clientes;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClientEdit extends AppCompatActivity {
+
+    private Button upload, cancel;
+    private EditText encargado, direccion;
+    private TextView nombre;
+
+    static DatabaseReference mRootReference;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_edit);
+
+        mRootReference = FirebaseDatabase.getInstance().getReference();
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("name");
+
+        //Enlace de UI
+        nombre = findViewById(R.id.clientName);
+        nombre.setText(id);
+
+        encargado = findViewById(R.id.clientEncargado);
+        direccion = findViewById(R.id.clientDireccion);
+        upload = findViewById(R.id.clientUpload);
+        cancel = findViewById(R.id.clientCancel);
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                uploadClient(id);
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+
+
+    public void uploadClient(String nombre){
+
+        String employee = encargado.getText().toString();
+        String direction = direccion.getText().toString();
+
+        if (employee.isEmpty() || direction.isEmpty()){
+            Toast.makeText(getApplicationContext(), "Ingrese todos los campos.",
+                    Toast.LENGTH_LONG).show();
+        }else {
+            // Llamada al metodo que sube los datos a la db
+            uploadData(nombre,employee, direction);
+        }
+    }
+
+    private void uploadData(String nombre, String employee, String direction) {
+        // Hash donde se almacenan los datos a subir
+        Map<String, Object> datosClientes = new HashMap<>();
+
+        // Insercion de los datos en el hash
+        datosClientes.put("nombre", nombre);
+        datosClientes.put("encargado", employee);
+        datosClientes.put("direccion", direction);
+
+        // Se crea un hijo (similar a una tabla) y se ingresan los valores
+        mRootReference.child("Clientes").child(nombre).setValue(datosClientes);
+
+        // Notificacion Toast para mostrar si el cliente fue cargado
+        Toast.makeText(getApplicationContext(), "Cliente cargado exitosamente.",
+                Toast.LENGTH_LONG).show();
+        Clientes.list.clear();
+        finish();
+    }
+}
--- app/src/main/java/com/example/pedidosapp/clientsLogic/Adapter.java
+++ app/src/main/java/com/example/pedidosapp/clientsLogic/Adapter.java
@@ -1,30 +1,48 @@
 package com.example.pedidosapp.clientsLogic;
 
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.pedidosapp.ClientEdit;
+import com.example.pedidosapp.Inicio;
 import com.example.pedidosapp.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.example.pedidosapp.tabs.Clientes;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.model.Document;
+
 
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
     Context context;
-
+    DatabaseReference databaseReference;
 
     ArrayList<Client> list;
 
@@ -61,12 +79,24 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
                 list.clear();
             }
         });
+
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, ClientEdit.class);
+                intent.putExtra("name", client.getNombre());
+                context.startActivity(intent);
+            }
+        });
     }
+
 
     @Override
     public int getItemCount() {
         return list.size();
     }
+
+
 
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
@@ -80,6 +110,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
             encargado = itemView.findViewById(R.id.clientEncargado);
             direccion = itemView.findViewById(R.id.clientDireccion);
             delete = itemView.findViewById(R.id.clientDelete);
+            edit = itemView.findViewById(R.id.clientEdit);
 
 
         }
